16e80f508498393272b997eea258cb5ad64ac71d
Tobias Weber <toby-weber@gmx.de>
2024-11-25 21:02:19 +0100

    add ItermDto

---
+++ frontend/src/models/ItemDto.ts
@@ -0,0 +1,10 @@
+import {ItemCategory} from "./ItemCategory.ts";
+import {ItemStatus} from "./ItemStatus.ts";
+
+export type ItemDto = {
+    name: string,
+    img: string,
+    description: string,
+    category: ItemCategory,
+    status: ItemStatus
+}
