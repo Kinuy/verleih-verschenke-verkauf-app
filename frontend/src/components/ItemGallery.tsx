c1ed4ee0361dec46dfb3baab591ecc855054b466
Tobias Weber <toby-weber@gmx.de>
2024-11-25 17:13:07 +0100

    add item gallery to show all items

--- frontend/src/components/ItemGallery.tsx
+++ frontend/src/components/ItemGallery.tsx
@@ -1,13 +1,29 @@
-
-
+import {useEffect, useState} from "react";
+import {Item} from "../models/Item.ts";
+import axios from "axios";
+import ItemCard from "./ItemCard.tsx";
+import "./ItemGallery.css";
 
 export default function ItemGallery() {
+    const [items, setItems] = useState<Item[]>()
 
-    function
+    function fetchAllItems(){
+        axios.get("/api/item")
+            .then((response)=>setItems(response.data))
+            .catch(error => {console.error("Error fetching item list:", error);
+        });
+    }
+
+    useEffect(() => {
+        fetchAllItems()
+    }, []);
 
     return (
-        <div>
-
+        <div className= "ItemGallery-container">
+            <h2>My Items</h2>
+            {
+                items?.map((element)=>{return <ItemCard key={element.id} item={element}/>})
+            }
         </div>
     );
 };
