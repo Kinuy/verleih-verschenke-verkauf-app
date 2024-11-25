a7db2e885418793797d6660037f86b0a1eb6ae35
Tobias Weber <toby-weber@gmx.de>
2024-11-25 11:42:12 +0100

    Dto included

--- backend/src/main/java/springweb/backend/model/Item.java
+++ backend/src/main/java/springweb/backend/model/Item.java
@@ -1,10 +1,15 @@
 package springweb.backend.model;
 
+import lombok.Builder;
+import lombok.With;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document("items")
+@With
+@Builder
 public record Item(
-        String id,
+        @Id String id,
         String name,
         String img,
         String description,
