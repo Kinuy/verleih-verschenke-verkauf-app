8e89b8af3163638a7d2d38d4c3bd88cf2f4f40ee
Tobias Weber <toby-weber@gmx.de>
2024-11-26 17:45:26 +0100

    add home

---
+++ frontend/src/components/Home.tsx
@@ -0,0 +1,8 @@
+import "./Home.css"
+export default function Home() {
+    return (
+        <div className="home-container">
+            <h1>Welcome <br/> to my storage.</h1>
+        </div>
+    );
+}
