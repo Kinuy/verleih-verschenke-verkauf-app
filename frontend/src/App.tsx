56698044c45f8e9866b190663edc9ee5eaeceb75
Tobias Weber <toby-weber@gmx.de>
2024-11-26 17:43:28 +0100

    add routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,21 +3,22 @@ import "./models/ItemCategory.ts";
 import NewItemCard from "./components/NewItemCard.tsx";
 import ItemGallery from "./components/ItemGallery.tsx";
 import "./App.css";
+import Header from "./components/Header.tsx";
+import {Route, Routes} from "react-router";
+import Home from "./components/Home.tsx";
 
 
 
 export default function App() {
-
-
-
     return (
         <div>
-            <h1>Fuer Umme / Im Keller wirds wieder heller....App</h1>
-            <div className="landing-container">
-                <NewItemCard/>
-                <ItemGallery/>
-            </div>
-
+            <Header/>
+            <Routes>
+                <Route path={"/"} element={<Home/>}/>
+                <Route path={"/manage"} element={<NewItemCard/>}/>
+                <Route path={"/storage"} element={<ItemGallery/>}/>
+                <Route path={"*"} element={<p>Page not found!</p>}/>
+            </Routes>
         </div>
     )
 }
