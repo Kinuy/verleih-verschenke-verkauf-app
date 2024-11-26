a5c416c7a6caa57374a4cd74f11a6cfc1bf6e25e
Tobias Weber <toby-weber@gmx.de>
2024-11-26 17:47:02 +0100

    KISS

--- frontend/src/components/ItemCard.tsx
+++ frontend/src/components/ItemCard.tsx
@@ -9,11 +9,8 @@ type Props = {
 export default function ItemCard(props: Props) {
     return (
         <div className="itemCard-container">
-            <img src={""} alt="item image"/>
             <div className="itemCard-tags">
                 <p>{props.item.name}</p>
-                <p>{props.item.category}</p>
-                <p>{props.item.status}</p>
             </div>
         </div>
     );
