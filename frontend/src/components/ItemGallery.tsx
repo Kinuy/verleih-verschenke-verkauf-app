4f3608fc8ee6c1e35031bdfbb7bd1d24e9a4b760
Tobias Weber <toby-weber@gmx.de>
2024-11-26 10:29:50 +0100

    add search bar and filter option

--- frontend/src/components/ItemGallery.tsx
+++ frontend/src/components/ItemGallery.tsx
@@ -3,27 +3,70 @@ import {Item} from "../models/Item.ts";
 import axios from "axios";
 import ItemCard from "./ItemCard.tsx";
 import "./ItemGallery.css";
+import ItemSearchBar from "./ItemSearchBar.tsx";
 
 export default function ItemGallery() {
-    const [items, setItems] = useState<Item[]>()
+    const [items, setItems] = useState<Item[]>([])
+    const [filteredItems, setFilteredItems] = useState<Item[]>([]);
 
     function fetchAllItems(){
         axios.get("/api/item")
-            .then((response)=>setItems(response.data))
+            .then((response)=>{
+                setItems(response.data);
+                setFilteredItems(response.data);
+            })
             .catch(error => {console.error("Error fetching item list:", error);
         });
     }
+
+    const filterItems = (query: string) => {
+        if (query.trim() === "") {
+            setFilteredItems(items);
+            console.log("ok")
+        } else {
+            const lowercasedQuery = query.toLowerCase();
+            const filtered = items.filter((item) =>
+                item.name.toLowerCase().startsWith(lowercasedQuery)
+            );
+            setFilteredItems(filtered);
+            console.log("ja")
+        }
+    };
 
     useEffect(() => {
         fetchAllItems()
     }, []);
 
     return (
-        <div className= "ItemGallery-container">
-            <h2>My Items</h2>
+        <div>
+            <h2>Stuff</h2>
+            <ItemSearchBar onSearch={filterItems}/>
+            <div className= "ItemGallery-container">
             {
-                items?.map((element)=>{return <ItemCard key={element.id} item={element}/>})
+                filteredItems.map((element)=>{
+                    if (element.status === "TO_SELL") {
+                        return (
+                            <div key={element.id} className="itemCardStatusSell-container">
+                                <ItemCard item={element} />
+                            </div>
+                        );
+                    } else if (element.status === "TO_LEND") {
+                        return (
+                            <div key={element.id} className="itemCardStatusLend-container">
+                                <ItemCard item={element} />
+                            </div>
+                        );
+                    } else if (element.status === "TO_GIVE_AWAY") {
+                        return (
+                            <div key={element.id} className="itemCardStatusGive-container">
+                                <ItemCard item={element} />
+                            </div>
+                        );
+                    } else {
+                        return null;
+                    }})
             }
+            </div>
         </div>
     );
 };
