bd1772012bec7c3d3310c02acbb78f1712f2bfef
Tobias Weber <toby-weber@gmx.de>
2024-12-03 18:18:12 +0100

    add delete Image method

--- backend/src/main/java/springweb/backend/service/ImageService.java
+++ backend/src/main/java/springweb/backend/service/ImageService.java
@@ -19,7 +19,7 @@ public class ImageService {
 
     public String uploadImage(MultipartFile image) throws IOException {
 
-        File fileToUpload = File.createTempFile("file", "");
+        File fileToUpload = File.createTempFile("item", "");
         image.transferTo(fileToUpload);
 
 
@@ -27,5 +27,19 @@ public class ImageService {
         return response.get("secure_url").toString();
     }
 
+    public void deleteImage(String imageUrl) {
+        String publicId = extractPublicIdFromUrl(imageUrl);
+
+        try {
+            cloudinary.uploader().destroy(publicId, Collections.emptyMap());
+        } catch (IOException e) {
+            throw new RuntimeException("Error, can not delete image.", e);
+        }
+    }
+    public String extractPublicIdFromUrl(String url) {
+        String[] parts = url.split("/");
+        String publicId = parts[parts.length - 1].split("\\.")[0];
+        return publicId;
+    }
 }
 
