8f4d72e98c975767a05946cdd60fb181f27a9769
Kinuy <toby-weber@gmx.de>
2024-12-11 09:35:32 +0100

    some cleanup

--- frontend/src/components/MapCard.tsx
+++ frontend/src/components/MapCard.tsx
@@ -1,89 +1,30 @@
-import "./MapCard.css";
-
+import "./MapCard.css"
 import "leaflet/dist/leaflet.css";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import MarkerClusterGroup from "react-leaflet-cluster";
+import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
+import {Icon} from "leaflet";
+import {Item} from "../models/Item.ts";
 
-import { Icon, divIcon, point } from "leaflet";
-
-// create custom icon
 const customIcon = new Icon({
-    iconUrl: "https://cdn-icons-png.flaticon.com/512/447/447031.png",
-    //iconUrl: require("./icons/placeholder.png"),
+    iconUrl: "https://cdn-icons-png.flaticon.com/128/2776/2776067.png",
     iconSize: [38, 38] // size of the icon
 });
 
-// custom cluster icon
-const createClusterCustomIcon = function (cluster) {
-    return new divIcon({
-        html: `<span class="cluster-icon">${cluster.getChildCount()}</span>`,
-        className: "custom-marker-cluster",
-        iconSize: point(33, 33, true)
-    });
-};
+type Props = {
+    item: Item;
+}
 
-// markers
-const markers = [
-    {
-        geocode: [48.86, 2.3522],
-        popUp: "Hello, I am pop up 1"
-    },
-    {
-        geocode: [48.85, 2.3522],
-        popUp: "Hello, I am pop up 2"
-    },
-    {
-        geocode: [48.855, 2.34],
-        popUp: "Hello, I am pop up 3"
-    }
-];
-
-export default function MapCard() {
+export default function MapCard(props:Props) {
     return (
-        <MapContainer center={[52.506615, 13.317524]} zoom={13}>
-            {/* OPEN STREEN MAPS TILES */}
+        <MapContainer center={[52.506615, 13.317524]} zoom={10}>
             <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution=""
+                url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            {/* WATERCOLOR CUSTOM TILES */}
-            {/* <TileLayer
-        attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg"
-      /> */}
-            {/* GOOGLE MAPS TILES */}
-            {/* <TileLayer
-        attribution="Google Maps"
-        // url="http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}" // regular
-        // url="http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}" // satellite
-        url="http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}" // terrain
-        maxZoom={20}
-        subdomains={["mt0", "mt1", "mt2", "mt3"]}
-      /> */}
-
-            <MarkerClusterGroup
-                chunkedLoading
-                iconCreateFunction={createClusterCustomIcon}
-            >
-                {/* Mapping through the markers */}
-                {markers.map((marker) => (
-                    <Marker position={marker.geocode} icon={customIcon}>
-                        <Popup>{marker.popUp}</Popup>
-                    </Marker>
-                ))}
-
-                {/* Hard coded markers */}
-                {/* <Marker position={[51.505, -0.09]} icon={customIcon}>
-          <Popup>This is popup 1</Popup>
-        </Marker>
-        <Marker position={[51.504, -0.1]} icon={customIcon}>
-          <Popup>This is popup 2</Popup>
-        </Marker>
-        <Marker position={[51.5, -0.09]} icon={customIcon}>
-          <Popup>This is popup 3</Popup>
-        </Marker>
-       */}
-            </MarkerClusterGroup>
+            <Marker position={props.item.geocode} icon={customIcon}>
+                <Popup>{props.item.name}</Popup>
+            </Marker>
         </MapContainer>
+
+
     );
 }
