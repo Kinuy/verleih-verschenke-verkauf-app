dcdce3a22f6b236a57716a3dd810f3c0cbfe8df7
Kinuy <toby-weber@gmx.de>
2024-12-16 21:52:02 +0100

    disable csrf

--- backend/src/main/java/springweb/backend/security/SecurityConfig.java
+++ backend/src/main/java/springweb/backend/security/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                //.csrf(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
                 .authorizeHttpRequests(a->a
                         .requestMatchers("/api/admin/**").hasRole(AppUserRole.ADMIN.name())
