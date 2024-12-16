b26883eab1feaed91a2df9ff5584a4972e66e8fc
Kinuy <toby-weber@gmx.de>
2024-12-16 09:57:10 +0100

    add account link

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,12 +1,18 @@
 import {Link, NavLink} from "react-router-dom";
 import "./Header.css";
+
+
 export default function Header() {
+
+
+
     return (
         <div>
             <nav className="navbar">
                 <Link className="navbar-link" to={"/"}>Home</Link>
                 <NavLink className="navbar-link" to={"/manage"}>Manage</NavLink>
                 <NavLink className="navbar-link" to={"/storage"}>Storage</NavLink>
+                <NavLink className="navbar-link" to={"/account"}>Account</NavLink>
             </nav>
         </div>
     );
