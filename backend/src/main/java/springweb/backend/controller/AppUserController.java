9c8ad0137c60abd7d286ad259320bf131bae2d09
Kinuy <toby-weber@gmx.de>
2024-12-16 14:37:27 +0100

    rename AppUserDetails to AppUserResponse

--- backend/src/main/java/springweb/backend/controller/AppUserController.java
+++ backend/src/main/java/springweb/backend/controller/AppUserController.java
@@ -6,7 +6,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.*;
 import springweb.backend.model.AppUser;
-import springweb.backend.model.AppUserProfile;
+import springweb.backend.model.AppUserResponse;
 import springweb.backend.model.AppUserRegisterDto;
 import springweb.backend.service.AppUserService;
 
@@ -18,22 +18,22 @@ public class AppUserController {
     private final AppUserService appUserService;
 
     @GetMapping("me")
-    public AppUserProfile getCurrentUser(@AuthenticationPrincipal User user) {
+    public AppUserResponse getCurrentUser(@AuthenticationPrincipal User user) {
         System.out.println(user);
         AppUser appUser = appUserService.getUserByUsername(user.getUsername());
-        return new AppUserProfile(appUser.id(), appUser.username(), appUser.role());
+        return new AppUserResponse(appUser.id(), appUser.username(), appUser.role());
     }
 
     @PostMapping
-    public AppUserProfile register(@RequestBody AppUserRegisterDto appUserRegisterDto) throws BadRequest {
+    public AppUserResponse register(@RequestBody AppUserRegisterDto appUserRegisterDto) throws BadRequest {
         if(appUserService.userExistsByUsername(appUserRegisterDto.username())){
             throw new BadRequest("Username already exists");
         }
         else{
             AppUser appUser = appUserService.addUser(appUserRegisterDto);
-            return new AppUserProfile(appUser.id(), appUser.username(), appUser.role());
+            return new AppUserResponse(appUser.id(), appUser.username(), appUser.role());
         }
-
-
     }
+
+
 }
