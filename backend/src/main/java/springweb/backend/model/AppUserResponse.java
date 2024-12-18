87f18f5a2775575a6763da43abd3f8b60953d15f
Kinuy <toby-weber@gmx.de>
2024-12-18 10:20:17 +0100

    add list

--- backend/src/main/java/springweb/backend/model/AppUserResponse.java
+++ backend/src/main/java/springweb/backend/model/AppUserResponse.java
@@ -1,8 +1,11 @@
 package springweb.backend.model;
 
+import java.util.List;
+
 public record AppUserResponse(
         String id,
         String username,
-        AppUserRole role
+        AppUserRole role,
+        List<String> items
 ) {
 }
