ad2e24b807fc91cd9cd8610e0abc9bfb06a78986
Kinuy <toby-weber@gmx.de>
2024-12-18 10:21:05 +0100

    change returns to AppUserResponse

--- backend/src/main/java/springweb/backend/service/AppUserService.java
+++ backend/src/main/java/springweb/backend/service/AppUserService.java
@@ -1,10 +1,13 @@
 package springweb.backend.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import springweb.backend.model.AppUser;
 import springweb.backend.model.AppUserRegisterDto;
+import springweb.backend.model.AppUserResponse;
 import springweb.backend.model.AppUserRole;
 import springweb.backend.repository.AppUserRepository;
 
@@ -19,22 +22,33 @@ public class AppUserService{
     private final PasswordEncoder passwordEncoder;
 
 
-    public AppUser addUser(AppUserRegisterDto appUserRegisterDto) {
-        AppUser appUser = new AppUser(null,
+    public AppUserResponse getLoggedInUser2() {
+        var principal = (User)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        AppUser appUser =  getUserByUsername(principal.getUsername());
+        return new AppUserResponse(appUser.id(),appUser.username(),appUser.role(),appUser.items());
+    }
+
+    public AppUserResponse getLoggedInUser(User user) {
+        AppUser appUser =  getUserByUsername(user.getUsername());
+        return new AppUserResponse(appUser.id(),appUser.username(),appUser.role(),appUser.items());
+    }
+
+    public AppUser getUserByUsername(String username) {
+        return userRepo.findAppUserByUsername(username).orElseThrow(() -> new NoSuchElementException("User not found."));
+    }
+
+    public boolean userExistsByUsername(String username) {
+        return userRepo.existsByUsername(username);
+    }
+
+    public AppUser register(AppUserRegisterDto appUserRegisterDto) {
+        AppUser appUser = new AppUser(
+                null,
                 appUserRegisterDto.username(),
                 passwordEncoder.encode(appUserRegisterDto.password()),
                 AppUserRole.USER,
                 Collections.emptyList()
         );
         return userRepo.save(appUser);
-    }
-
-    public AppUser getUserByUsername(String username) {
-        return userRepo.findByUsername(username).orElseThrow(() -> new NoSuchElementException("User with username: " + username + " does not exist!"));
-    }
-
-    public boolean userExistsByUsername(String username) {
-
-        return userRepo.existsByUsername(username);
     }
 }
